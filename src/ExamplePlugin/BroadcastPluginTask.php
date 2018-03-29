e6124cfdf6b596cc137d9bd8ce20391c0e1a1721
Jackthehaxk21 <gangnam253@gmail.com>
2018-03-29 17:30:48 +0100

    Base

---
+++ src/ExamplePlugin/BroadcastPluginTask.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ExamplePlugin;
+
+use pocketmine\Player;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\Server;
+
+class BroadcastPluginTask extends PluginTask{
+
+	public function onRun($currentTick){
+		Server::getInstance()->broadcastMessage("[ExamplePlugin] I've run on tick " . $currentTick);
+	}
+}
