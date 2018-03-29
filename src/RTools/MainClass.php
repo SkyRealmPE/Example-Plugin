8fefc4f12226eb754e77d990d250d9cb4ddf7534
Jackthehaxk21 <gangnam253@gmail.com>
2018-03-29 20:23:28 +0100

    NEW STATS

--- src/RTools/MainClass.php
+++ src/RTools/MainClass.php
@@ -12,20 +12,21 @@ use pocketmine\plugin\PluginBase;
 class MainClass extends PluginBase {
 
 	public function onLoad(){
-		$this->getLogger()->info(TextFormat::GREEN."I've been loaded!");
+		$this->getServer()->broadcastMessage(TextFormat::AQUA."RTools Loaded !");
+		$this->getLogger()->info(TextFormat::GREEN."+=========== RTOOLS ==========+")
+		$this->getLogger()->info(TextFormat::AQUA."| Name    :: RTools           |")
+		$this->getLogger()->info(TextFormat::AQUA."| Version :: 0.1.0            |")
+		$this->getLogger()->info(TextFormat::AQUA."| Author  :: Jackthehack21    |")
+		$this->getLogger()->info(TextFormat::GREEN."+=============================+")
 	}
 
 	public function onDisable(){
-		$this->getLogger()->info(TextFormat::RED."I've been disabled!");
+		$this->getServer()->broadcastMessage(TextFormat::PURPLE."RTools Disabled !");
 	}
 
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
 		$commandName = $command->getName();
 		if($commandName == "ping"){
-			while(true) {
-				$this->getServer()->broadcastMessage("Coming Soon !");
-				sleep(3);
-			};
 			$sender->sendMessage(TextFormat::BLUE."Pong!");
 		    return true;
 		}
