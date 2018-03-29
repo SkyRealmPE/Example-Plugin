ff97fb4246d20d37b48818c626b1ece8ad9eeba4
Jackthehaxk21 <gangnam253@gmail.com>
2018-03-29 19:56:14 +0100

    new broadcasts

--- src/RTools/MainClass.php
+++ src/RTools/MainClass.php
@@ -12,18 +12,21 @@ use pocketmine\plugin\PluginBase;
 class MainClass extends PluginBase {
 
 	public function onLoad(){
-		$this->getLogger()->info("I've been loaded!");
+		$this->getLogger()->info(TextFormat::GREEN."I've been loaded!");
+		setInterval(function(){
+			$this->owner->getServer()->broadcastMessage("Coming Soon !");
+		}, 6000)
 	}
 
 	public function onDisable(){
-		$this->getLogger()->info("I've been disabled!");
+		$this->getLogger()->info(TextFormat::RED."I've been disabled!");
 	}
 
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
 		$commandName = $command->getName();
-		if($commandName === "ping"){
-		  $sender->sendMessage(TextFormat::BLUE."Pong!");
-		  return true;
+		if($commandName == "ping"):
+			$sender->sendMessage(TextFormat::BLUE."Pong!");
+		    return true;
 		}
 		return false;
 	}
